f9a22426e19f88b0f496764ede0a7fd62cb8f199
Kevin <kevinjmathewskj@gmail.com>
2026-02-25 00:35:22 +0530

    bronze layer

--- netflix_snowflake_dbt/models/bronze/bronze_tags.sql
+++ netflix_snowflake_dbt/models/bronze/bronze_tags.sql
@@ -3,7 +3,7 @@
 select 
     userId as user_id,
     movieId as movie_id,
-    tag,
+    lower(trim(regexp_replace(tag, '[^a-zA-Z0-9]', ' '))) as tag,
     to_timestamp(timestamp) as tag_timestamp,
     current_timestamp() as _loaded_at
 from {{ source('raw', 'raw_tags') }}
